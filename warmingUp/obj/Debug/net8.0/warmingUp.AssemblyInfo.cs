5c497e7f7c230d7a886ec573dc5b7630010855d2
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-10-18 16:29:26 +0400

    Initial Commit for Task1V1

--- warmingUp/obj/Debug/net8.0/warmingUp.AssemblyInfo.cs
+++ warmingUp/obj/Debug/net8.0/warmingUp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92b26590b7a23ec7fe760aa3ed64fd2784be5b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59740f8bc9653208d7ef237d3564ff8d22189874")]
 [assembly: System.Reflection.AssemblyProductAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
