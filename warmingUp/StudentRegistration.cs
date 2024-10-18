3305a598122b54ff11b39da08d12c97960263c4a
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-10-18 14:22:26 +0400

    Task2 is completed

---
+++ warmingUp/StudentRegistration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warmingUp
+{
+    internal class StudentRegistration
+    {
+        private string _lastName;
+        private string _firstName;
+        private int id;
+
+        private event EventHandler studentRegistered;
+
+        public StudentRegistration(string lastName, string firstName , int id) 
+        {
+            _lastName = lastName;
+            _firstName = firstName;
+            this.id = id;
+
+            RegisterStudent();
+        }
+
+        public void RegisterStudent() 
+        {
+
+            studentRegistered += SubscriberMethod;
+            OnStudentRegistered();
+        }
+
+        private void OnStudentRegistered() 
+        {
+            studentRegistered?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void SubscriberMethod(object sender , EventArgs e) 
+        {
+            Console.WriteLine($"You have been Registered Well Done \nTime of the registration: {DateTime.Now:HH.mm.ss}");
+        }
+    }
+}
