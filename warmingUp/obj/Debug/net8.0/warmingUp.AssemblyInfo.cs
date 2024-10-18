fcc9864aa48570920876d4a643249c1167a5136e
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-10-18 13:31:34 +0400

    first exercise finished

--- warmingUp/obj/Debug/net8.0/warmingUp.AssemblyInfo.cs
+++ warmingUp/obj/Debug/net8.0/warmingUp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15073d278173ecaa03aef5805ac360a5c458bd1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("warmingUp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
