3f7d60e0540e790830eb5f9440007b2ad0a5fb3a
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-10-29 13:17:44 +0400

    davaleba35 finished

---
+++ davaleba35/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba35
+{
+    internal class Product
+    {
+        public int ItemId { get; set; } 
+        public string ItemDes { get; set; }
+    }
+}
---
+++ davaleba35/Purchase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba35
+{
+    internal class Purchase
+    {
+        public int InvoiceNu {  get; set; }
+        public int ItemId {  get; set; }
+        public int PurchaseQuantity {  get; set; }
+
+    }
+}
