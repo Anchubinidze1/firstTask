c0b6e2589e032c275d097922e2dcff720b84e4c1
Anri Chubinidze <anrichubinidze204@gmail.com>
2024-11-07 12:59:08 +0400

    Initial commit

---
+++ GCExercises/Program.cs
@@ -0,0 +1,125 @@
+﻿
+
+using System;
+using System.IO;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace GCExercises 
+{
+
+    public class TimedLogWriter : IDisposable
+    {
+        private StreamWriter _writer;
+        private Timer _timer;
+        private bool _disposed = false;
+
+        public TimedLogWriter(string filePath, double intervalSeconds) 
+        {
+            _writer = new StreamWriter(filePath, append: true);
+            _timer = new Timer(intervalSeconds * 1000);
+            _timer.Elapsed += OnTimedEvent;
+            _timer.Start();
+        }
+
+        public void WriteLog(string message)
+        {
+            if (_disposed)
+                throw new ObjectDisposedException("TimedLogWriter");
+
+            _writer.WriteLine($"{DateTime.Now}: {message}");
+            _writer.Flush();
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            WriteLog("Auto-log entry");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _timer?.Stop();
+                    _timer?.Dispose();
+                    _writer?.Dispose();
+                    Console.WriteLine("Resources were released.");
+                }
+                _disposed = true;
+            }
+        }
+
+        ~TimedLogWriter()
+        {
+            Dispose(false);
+        }
+    }
+
+    public class LogWrite : IDisposable
+    {
+        private FileStream _stream;
+        private bool _disposed =  false;
+
+        public LogWrite(string filePath) 
+        {
+            _stream = new FileStream(filePath , FileMode.OpenOrCreate);
+            Console.WriteLine("File was created");
+        }
+
+        public void WriteLog(string message) 
+        {
+            if (_disposed)
+                throw new ObjectDisposedException("UnmanagedResourceHolder");
+
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(message+"  "+DateTime.Now);
+            _stream.Write(data, 0, data.Length);
+            Console.WriteLine("Data written to file");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) 
+        {
+            if(!_disposed) 
+            {
+                if(disposing) 
+                {
+                    _stream?.Dispose();
+                    Console.WriteLine("Memory was released");
+                }
+
+                _disposed = true;
+            }
+        }
+
+        ~LogWrite() 
+        {
+            Dispose(false);
+        }
+    }
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            using (var logWriter = new LogWrite("log.txt"))
+            {
+                logWriter.WriteLog("This is the first log entry.");
+                logWriter.WriteLog("This is the second log entry.");
+            }
+
+            Console.WriteLine("Log entries written and file closed.");
+        }
+    }
+}
---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,180 @@
+﻿
+
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace GarbageCollection
+{
+
+    public class UnmanagedResourceHolder : IDisposable
+    {
+
+        private IntPtr _unmanagedMemory;  // ფოინთერი არა მართვად რესურსზე
+        private bool _disposed =  false; // ცვლადი რომ შევამოწმოთ რესულრსები უკვე გათავისუფლდა თუ არ
+
+
+        public UnmanagedResourceHolder(int size) 
+        {
+            _unmanagedMemory = Marshal.AllocHGlobal(size);
+            Console.WriteLine($"Allocated {size} bytes of unmanaged memory.");
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);    // გამოიძახებს Dispose მეთოდს True პარამეტრით
+
+            GC.SuppressFinalize(this); // დესტრუქტორი რომ აღარ გამოიძახოს რადგან უკვე მოგვარებულია რესურსების თემა
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    
+                }
+
+                if(_unmanagedMemory != IntPtr.Zero ) 
+                {
+                    Marshal.FreeHGlobal(_unmanagedMemory);  // შიგნით არსებული მეთოდი რომელიც გაათავისუფლებს ამ რესურს
+                    _unmanagedMemory = IntPtr.Zero;
+                    Console.WriteLine("Unmanaged memory freed.");
+                }
+
+                _disposed = true;
+            }
+        }
+
+
+        ~UnmanagedResourceHolder()
+        {
+            Dispose(false); 
+        }
+
+        public void UseUnmanagedResource()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException("UnmanagedResourceHolder");
+
+            
+            Marshal.WriteByte(_unmanagedMemory, 0, 42);
+            Console.WriteLine("Wrote to unmanaged memory.");
+        }
+
+    }
+
+    public class SampleResource : IDisposable
+    {
+
+        private FileStream _stream;     // მართვადი რესურსი , მართვადი რატომაა მერე ავხსი
+        private bool _disposed =  false; // ცვლადი რომ შევამოწმოთ რესულრსები უკვე გათავისუფლდა თუ არა
+        public void Dispose()
+        {
+            Dispose(true);    // გამოიძახებს Dispose მეთოდს True პარამეტრით
+
+            GC.SuppressFinalize(this); // დესტრუქტორი რომ აღარ გამოიძახოს რადგან უკვე მოგვარებულია რესურსების თემა
+        }
+
+        public SampleResource(string filepath)  // მეთოდი რომელიც შექმნის ან გამოიძახებს ფაილს
+        {
+            _stream = new FileStream(filepath, FileMode.OpenOrCreate);
+            Console.WriteLine("File opened");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _stream?.Dispose();
+                    Console.WriteLine("Managed resources disposed.");
+                }
+
+                
+
+                _disposed = true;
+            }
+        }
+
+        public void WriteToFile(string text)  // ფაილში ჩაწერის ლოგიკა
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException("SampleResource");
+            }
+
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(text);
+            _stream.Write(data, 0, data.Length);
+            Console.WriteLine("Data written to file");
+
+        }
+
+        ~SampleResource()
+        {
+            Dispose(false); // გამოიძახებს GC თუ dispose() მეთოდი არ იქნა გამოძახებული თავის დაზღვევის საშვალებაა        
+        }
+
+
+       
+
+    }
+
+
+    public class MyResorce : IDisposable
+    {
+        private bool _disposed = false;  // ცვლადი რომ შევამოწმოთ რესულრსები უკვე გათავისუფლდა თუ არა
+
+        public void Dispose()
+        {
+            Dispose(true);    // გამოიძახებს Dispose მეთოდს True პარამეტრით
+
+            GC.SuppressFinalize(this); // დესტრუქტორი რომ აღარ გამოიძახოს რადგან უკვე მოგვარებულია რესურსების თემა
+        }
+
+
+        protected virtual void Dispose(bool disposing) 
+        {
+            if (!_disposed) 
+            {
+                if (disposing) 
+                {
+                         // ლოგიკა რომელიც მოუვლის მართვად რესურსებს
+                }            
+
+                //ლოგიკა რომელიც მოუვლის არა მართვად რესურსებს
+
+                _disposed = true;
+            }
+        }
+
+        ~MyResorce() 
+        {
+            Dispose(false); // გამოიძახებს GC თუ dispose() მეთოდი არ იქნა გამოძახებული თავის დაზღვევის საშვალებაა        
+        }
+    }
+
+    class Program
+    {
+
+        public static void Main(string[] args) 
+        {
+            /*
+            SampleResource resource = new SampleResource("Test1");
+            resource.WriteToFile("Helloo");
+            resource.Dispose();
+            */
+
+            UnmanagedResourceHolder unmanagedResourceHolder = new UnmanagedResourceHolder(23);
+
+            unmanagedResourceHolder.UseUnmanagedResource();
+
+            unmanagedResourceHolder.Dispose();
+
+
+        }
+    }
+}
